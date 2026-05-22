24316780aef8dc26a25ff56af1d83307ac8a2c95
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-05-22 23:49:56 +0600

    Implemented Map for showing all category name. Dynammically handle the category ID and successfully provided that param id into pages/CategoryNews

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,22 @@
 import { use } from "react";
+import { NavLink } from "react-router";
 
 const categoryPromise = fetch('/categories.json')
     .then(res => res.json())
 const Categories = () => {
     const categories = use(categoryPromise);
-    console.log(categories);
+
     return (
         <div>
             <h2 className='font-bold text-primary text-lg'>All Categories</h2>
+            <div className="grid grid-cols-1 mt-5 gap-3">
+                {
+                    categories.map(category => <NavLink className={"btn bg-base-100 text-base-content font-semibold border-0 hover:bg-base-200"}
+                        key={category.id}
+                        to={`/category/${category.id}`}
+                    >{category.name}</NavLink>)
+                }
+            </div>
         </div>
     );
 };
