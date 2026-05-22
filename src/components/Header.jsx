21336be02415f7022eeac9a11b5ad63326032f24
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-05-22 20:34:30 +0600

    50-2 Theme Customization, Font & Date-FNS Integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
+import { format } from 'date-fns';
 import logo from '../assets/logo.png';
 
 const Header = () => {
     return (
-        <div className='flex justify-center items-center flex-col gap-5 py-12'>
-            <img src={logo} alt="Dragon news" />
-            <h4 className='text-lg text-gray-600 font-semibold'>Journalism Without Fear or Favour</h4>
-            <p className='text-xl text-gray-500 font-medium'>Sunday, November 27, 2025</p>
+        <div className='flex justify-center items-center flex-col gap-4 py-12'>
+            <img src={logo} alt="Dragon news" className='w-117' />
+            <h4 className='text-lg text-accent font-semibold'>Journalism Without Fear or Favour</h4>
+            <p className='text-xl text-accent font-medium'><span className="text-primary">{format(new Date(), "EEEE")}</span>, {format(new Date(), "MMMM, d, Y")}</p>
         </div>
     );
 };
